0570cdf6016ad45bd26e317a7ae4cb1f66f67370
mariacavalc <mariaacavalc@gmail.com>
2022-06-06 13:13:12 -0400

    Repository

--- app/src/main/java/com/madu/to_doapp/TaskDatabase.java
+++ app/src/main/java/com/madu/to_doapp/TaskDatabase.java
@@ -1,10 +1,13 @@
 package com.madu.to_doapp;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = Task.class, version = 1)
 public abstract class TaskDatabase extends RoomDatabase {
@@ -13,14 +16,41 @@ public abstract class TaskDatabase extends RoomDatabase {
 
     public abstract TaskDao taskDao();
 
-    public static  synchronized TaskDatabase getInstance(Context context){
+    public static synchronized TaskDatabase getInstance(Context context){
         if (instance == null){
             instance = Room.databaseBuilder(context.getApplicationContext(),
                     TaskDatabase.class,
                     "task_database")
                     .fallbackToDestructiveMigration()
+                    .addCallback(roomCallback)
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void>{
+        private TaskDao taskDao;
+
+        private PopulateDbAsyncTask(TaskDatabase db){
+            this.taskDao = db.taskDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            taskDao.insert(new Task(
+                    "Levar milly para passear",
+                    "Andar por pelo menos 20 min",
+                    2,
+                    false));
+            return null;
+        }
     }
 }
